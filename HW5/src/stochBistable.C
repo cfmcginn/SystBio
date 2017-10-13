2aa13b4628a1ce312795f4f6f73ae3ebd2d146ba
Chris McGinn <cfmcginn@mit.edu>
2017-10-13 18:03:40 -0400

    Added base code for HW5

---
+++ HW5/src/stochBistable.C
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int stochBistable()
+{
+  
+
+  return 0;
+}
+
+int main()
+{
+  int retVal = 0;
+  retVal += stochBistable();
+  return retVal;
+}
---
+++ HW5/src/stochProteins.C
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int stochProteins()
+{
+  
+
+  return 0;
+}
+
+int main()
+{
+  int retVal = 0;
+  retVal += stochProteins();
+  return retVal;
+}
